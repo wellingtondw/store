60523906d68c8a8c13a61494254feaca8e240895
Wellington <wellington.map@hotmail.com>
2022-05-30 08:43:16 -0300

    config multer to supload images in disk storage

---
+++ api/src/config/multer.ts
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) =>
+    callback(null, __dirname + "../public/images"),
+  filename: (req, file, callback) =>
+    callback(null, file.fieldname + "-" + Date.now() + ".jpg"),
+});
+
+const upload = multer({ storage });
+
+export { upload };
